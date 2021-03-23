91f3c141f9e7d2d85b15d4aaeaed80320e874894
Ian Baker <ianbaker615@gmail.com>
2021-03-23 12:58:33 -0400

    Exercise 2.9 phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,48 +6,75 @@ const Contacts = () => {
 };
 
 const Contact = ({ contact }) => {
-  return <li>{contact.name}</li>;
+  return (
+    <li>
+      {contact.name} {contact.phone}
+    </li>
+  );
 };
 
 const App = () => {
   // state stuff
   const [contacts, setContacts] = useState([
-    { name: "Ian Baker", id: uuidv4() },
+    { name: "Ian Baker", phone: "6159574407", id: uuidv4() },
+    { name: "Emily Martin", phone: "7744884701", id: uuidv4() },
+    { name: "Tamara Baker", phone: "6158707720", id: uuidv4() },
+    { name: "Mark Baker", phone: "6158707722", id: uuidv4() },
   ]);
   const [newContact, setNewContact] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const [search, setSearch] = useState("");
 
   // event handlers
   const addContact = (e) => {
     // prevent form submission on change
     e.preventDefault();
     // check if name already in phonebook
-    if (contacts.some((contact) => contact.name === newContact))
+    if (contacts.some((contact) => contact.name === newContact)) {
       alert(`${newContact} is already in phonebook`);
-    else {
+    } else if (contacts.some((contact) => contact.phone === newPhone)) {
+      alert(`${newPhone} is already in phonebook`);
+    } else {
       // build contact object
       const contactObject = {
         name: newContact,
-        // number: ,
+        phone: newPhone,
         id: uuidv4(),
       };
 
       // append contactObject to persons array in state
       setContacts(contacts.concat(contactObject));
       setNewContact("");
+      setNewPhone("");
     }
   };
-
   const handleContactChange = (e) => {
     setNewContact(e.target.value);
   };
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value);
+  };
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   // the html that is returned
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input value={search} onChange={handleSearchChange} />
+      </div>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newContact} onChange={handleContactChange} />
+        </div>
+        <div>
+          phone number: <input value={newPhone} onChange={handlePhoneChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -56,9 +83,10 @@ const App = () => {
       <h2>Contacts</h2>
       <Contacts />
       <ul>
-        {contacts.map((contact) => (
+        {filteredContacts.map((contact) => (
           <Contact key={contact.id} contact={contact} />
         ))}
+        {contacts.filter((contact) => contact.name.includes({ search }))}
       </ul>
     </div>
   );
